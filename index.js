06e09aa9535fde6cbfaadf58abe5e4644cc4547b
Borisaye <boris_aroche@hotmail.com>
2023-03-30 20:45:00 -0400

    .

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply, random, mod, max, math;
+let num1 = 2;
+let num2 = 31;
+let num3 = 22;
+let num4 = 9;
+let randoNum;
+let numSet = [17,18,19,20] ;
 
-// Write your code here
+multiply = num1 * num2;
+
+random = Math.floor(Math.random() * 10) + 1;
+
+mod = num3 % num4;
+
+max = Math.max(...numSet);
